d3942d47b646a3b1343621de0a6cf654d3364161
aleynayilaz <yilazaleyna.56@gmail.com>
2021-11-05 19:15:33 +0300

    js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 
+const count = document.getElementById('count')
+
+let basketCount = 0
+
+count.innerHTML = basketCount
+const basketButtons = document.querySelectorAll('#basketButton')
+
+for(let i = 0; i < basketButtons.length; i++) {
+    basketButtons[i].addEventListener('click', function () {
+        basketCount++
+        count.innerHTML = basketCount
+        
+    })
+}
+
+
